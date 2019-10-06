f435d06137b0584b9103104a88592e623f94917c
nat13alia <natalia.krawczyk@microsoft.wsei.edu.pl>
2019-10-06 15:53:28 +0200

    Update app.js
    Solution to task "Zadanie 6"

--- 1_Zadania/obowiązkowe/2_Tablice/js/app.js
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -29,3 +29,50 @@ fruits.forEach(fruit => {
     console.log(fruit);
 })
 
+// Zadanie 3
+
+// function
+
+// Zadanie 4
+
+function multiply(array) {
+    let mulRes = array[0];
+    for(let i = 1; i < array.length; i++){
+        mulRes *= array[i];
+    }
+    console.log(mulRes);
+    return 
+}
+
+multiply([1,2,3,4,5,6,7])
+
+function getEvenAverage(array) {
+    let evenSum = 0;
+    let evenCounter = 0;
+    for(let i = 0; i < array.length; i++){
+        if(array[i] % 2 == 0){
+            evenSum += array[i];
+            evenCounter++;
+        }
+    }
+    let evenAvg = evenSum / evenCounter;
+    if (evenCounter == 0){
+        return null;
+    }
+    else {
+        return evenAvg;
+    }
+    console.log(evenAvg);
+}
+
+getEvenAverage([1,2,3,4,5,6,7])
+getEvenAverage([1,1,1,1])
+
+// Zadanie 6
+function sortArray(array) {
+    array.sort((a, b) => a-b);
+    console.log(array);
+    return array;
+}
+
+sortArray([145,11,3,64,4,6,10])
