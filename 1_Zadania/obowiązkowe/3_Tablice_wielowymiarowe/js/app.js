b4a2582bcc9e2a56f8a3820404dc3114084788e6
nat13alia <natalia13krawczyk@gmail.com>
2019-10-22 00:14:52 +0200

    Solution to '3_Tablice_wielowymiarowe - Zadanie 0'

--- 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
+++ 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
@@ -1,4 +1,3 @@
-
 var task1Array = [
     [2, 3],
     ["Ala", "Ola"],
@@ -6,7 +5,6 @@ var task1Array = [
     [5, 6, 7, 8],
     [12, 15, 67]
 ];
-
 
 var task2Array = [
     [1, 2, 3, 4],
@@ -17,9 +15,9 @@ var task2Array = [
 // Zadanie 0
 function checkArray(arr){
     let checked = [];
-    for(let i=0; i<arr.length; i++) {
+    for(let i = 0; i < arr.length; i++) {
         let check = true;
-        for(let j=0; j <arr[i].length; j++){
+        for(let j = 0; j < arr[i].length; j++){
             if(arr[i][j]%2 != 0){
                 check = false;
                 break;
@@ -39,53 +37,3 @@ var arr = [
     ];
 
 console.log(checkArray(arr));
-
-// Zadanie 1
-console.log(task1Array[3][2]);
-console.log(task1Array[1].length);
-console.log(task1Array[4][2]);
-
-// Zadanie 2
-
-for(let i = 0; i < task2Array.length; i++){
-    console.log(task2Array[i]);
-}
-
-for(let i = 0; i < task2Array.length; i++){
-    console.log(task2Array[i].length);
-}
-
-for(let i = 0; i < task2Array.length; i++){
-    for (let j = 0; j < task2Array.length; j++)
-        console.log(task2Array[i][j]);
-}
-
-// Zadanie 3
-
-function print2DArray(arr){
-    for(let i = 0; i < arr.length; i++){
-        for (let j = 0; j < arr.length; j++){
-            console.log(arr[i][j]);
-        }
-    }
-}
-
-// Zadanie 4
-var table = [[1,2], [0,4,6,8],[4,5,6,7,8]];
-print2DArray(table);
-
-// Zadanie 5
-function create2DArray(rows,columns){
-    let firstTable = [];
-    let number = 1;
-    for(let i = 0; i < rows; i++){
-        var secondTable = [];
-        for(let j = 0; j < columns; j++){
-            secondTable.push(number);
-            number++;
-        }
-        firstTable.push(secondTable);
-    }
-    return firstTable
-}
-console.log(create2DArray(4,4));
