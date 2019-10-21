7f462e75ca0d74e8a766328cc61494d37afc6207
nat13alia <natalia13krawczyk@gmail.com>
2019-10-21 22:52:14 +0200

    Solution to 'Funkcje czasu - Zadanie 0'

--- 1_Zadania/obowiązkowe/2_Tablice/js/zadanie02.js
+++ 1_Zadania/obowiązkowe/2_Tablice/js/zadanie02.js
@@ -2,7 +2,7 @@
 function createArray(number) {
     var newArray = [10];
 
-    for (var counter = 1; ; counter <= number) {
+    for (var counter = 1; counter <= number; counter++) {
         newArray.push(counter);
     }
 
--- 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
+++ 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
@@ -13,3 +13,79 @@ var task2Array = [
     [5, 6, 7, 8],
     [9, 10, 11, 12]
 ];
+
+// Zadanie 0
+function checkArray(arr){
+    let checked = [];
+    for(let i=0; i<arr.length; i++) {
+        let check = true;
+        for(let j=0; j <arr[i].length; j++){
+            if(arr[i][j]%2 != 0){
+                check = false;
+                break;
+            }
+        }
+        checked.push(check);
+    }
+    return checked;
+}
+
+var arr = [
+    [11, 12],
+    [42, 2],
+    [-4, -120],
+    [0, 0],
+    [1, 34],
+    ];
+
+console.log(checkArray(arr));
+
+// Zadanie 1
+console.log(task1Array[3][2]);
+console.log(task1Array[1].length);
+console.log(task1Array[4][2]);
+
+// Zadanie 2
+
+for(let i = 0; i < task2Array.length; i++){
+    console.log(task2Array[i]);
+}
+
+for(let i = 0; i < task2Array.length; i++){
+    console.log(task2Array[i].length);
+}
+
+for(let i = 0; i < task2Array.length; i++){
+    for (let j = 0; j < task2Array.length; j++)
+        console.log(task2Array[i][j]);
+}
+
+// Zadanie 3
+
+function print2DArray(arr){
+    for(let i = 0; i < arr.length; i++){
+        for (let j = 0; j < arr.length; j++){
+            console.log(arr[i][j]);
+        }
+    }
+}
+
+// Zadanie 4
+var table = [[1,2], [0,4,6,8],[4,5,6,7,8]];
+print2DArray(table);
+
+// Zadanie 5
+function create2DArray(rows,columns){
+    let firstTable = [];
+    let number = 1;
+    for(let i = 0; i < rows; i++){
+        var secondTable = [];
+        for(let j = 0; j < columns; j++){
+            secondTable.push(number);
+            number++;
+        }
+        firstTable.push(secondTable);
+    }
+    return firstTable
+}
+console.log(create2DArray(4,4));
