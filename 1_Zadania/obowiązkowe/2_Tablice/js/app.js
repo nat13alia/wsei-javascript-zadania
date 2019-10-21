1970a4670385687ae7178b1d9bb8ac583246e322
nat13alia <natalia13krawczyk@gmail.com>
2019-10-21 23:27:24 +0200

    Solution to '2_Tablice - Zadanie 1'

--- 1_Zadania/obowiązkowe/2_Tablice/js/app.js
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -20,3 +20,17 @@ const distFromAvarage = (arr) => {
 distFromAvarage([1,2,3,4,5,6,7]);
 distFromAvarage([1,1,1,1]);
 distFromAvarage([2,8,3,7]);
+
+// Zadanie 1
+
+const fruits = ["apple", "orange", "banana", "apricot", "strawberry"];
+
+console.log(fruits[0]);
+
+console.log(fruits[fruits.length - 1]);
+
+for(let i = 0; i < fruits.length; i++){
+    console.log(fruits[i]);
+}
+
+
