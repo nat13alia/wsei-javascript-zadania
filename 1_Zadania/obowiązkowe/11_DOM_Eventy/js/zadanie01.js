cc8598babdb05f4f50bd6e9b9dfa13e3097a04ce
nat13alia <natalia13krawczyk@gmail.com>
2020-01-22 12:12:57 +0100

    Solution to 'zadanie01'

--- 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie01.js
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie01.js
@@ -1,5 +1,14 @@
-var menu = document.querySelector("#menu");
-var paragraf = document.querySelector("p");
+document.addEventListener('DOMContentLoaded', function(e) {
+    var menu = document.querySelector("#menu");
+    var paragraf = document.querySelector("p");
+    menu.classList.add("menu");
+    paragraf.innerHTML = "A to jest paragraf w zadaniu 1";
+})
 
-menu.classList.add("menu");
-paragraf.innerHTML = "A to jest paragraf w zadaniu 1";
+
+// 1. Po przeniesieniu tagu <script> do sekcji <head> w konsoli pojawia się błąd: Uncaught TypeError: Cannot read property 'classList' of null
+// Wynika to z faktu iż umieszczając kod w tagu <head> powoduje to jego egzekucję, jeszcze zanim doszło do utworzenia elementów tagu <body>
+
+// 2. Po umieszczeniu kodu wewnątrz eventu DOMContentLoaded błąd znika, gdyż kod wykonuje się po załadowaniu treści dokumentu
+
+// 3. Po przeniesieniu tagu <script> z powrotem na koniec pliku, nie ma żadnej różnicy, kod wykonuje się poprawnie.
