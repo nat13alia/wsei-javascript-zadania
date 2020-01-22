1400bed5aa1a7402fe9c71802aa40e4baf8c5970
nat13alia <natalia13krawczyk@gmail.com>
2020-01-22 12:25:41 +0100

    Solution to 'zadanie02'

---
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie02.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(e){
+    const btn = document.querySelector('#mainBtn');
+    btn.addEventListener('click', function(e){
+        console.log('Hura! Działa!');
+    })
+})
