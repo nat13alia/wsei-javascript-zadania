9b5998945d67448791d5bf6b387db7bcf69bef54
nat13alia <natalia13krawczyk@gmail.com>
2020-01-31 15:19:23 +0100

    Solution to 'zadanie09'

--- 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie09.js
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie09.js
@@ -2,8 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   var a = document.getElementById("a");
   var b = document.getElementById("b");
 
-  // Tutaj this wskazuje na obiekt document (Ponieważ kontext wywołania funkcji
-  // addEventListener to właśnie document)
+  // Tutaj this wskazuje na obiekt document (Ponieważ kontext wywołania funkcji addEventListener to właśnie document)
   console.log("obiekt document: ", this);
 
   a.addEventListener("click", function (event) {
@@ -15,14 +14,12 @@ document.addEventListener("DOMContentLoaded", function () {
     // Tutaj this wskazuje na element DOM o id b
     console.log("b: ", this);
 
-
-    function innerFuncOne() {
-      // Tutaj this wskazuje na element Window, bo funkcja została
-      //wywołana bez żadnego kontekstu.
-      console.log("innerFuncOne: ", this);
-    }
-    innerFuncOne();
-
+    function innerFuncOne(b) {  
+      // b jest przekazane jako argument funkcji za pomocą parametru
+        b.style.backgroundColor = "blue"; // zmiana backgroundColor dla przekazanego do funkcji poprzez parametr argumentu
+        console.log("innerFuncOne: ", b.style.backgroundColor); // wyświetla informację
+      }
+      innerFuncOne(this); // this wskazuje na parametr funkcji - element b
   });
 
 })
